719f3e390eacffd46bbd12220f75d07cb424185b
Hugo Pacini <hugodpn@dhpn.com>
2010-04-21 00:13:10 -0300

    Add a little css and begin with user crud.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,17 @@
 class Admin::UsersController < ApplicationController
+
   def index
     permission_deny unless current_user.has_perm?("listing_users")
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @article }
+    end
+  end
+
 end
