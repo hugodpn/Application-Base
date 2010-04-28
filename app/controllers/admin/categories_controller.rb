bef9931822562b45b24ec2c6a193f7dd8bd77432
Hugo Pacini <hugodpn@dhpn.com>
2010-04-28 20:49:40 -0300

    Add permissions on categories.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,58 +1,78 @@
 class Admin::CategoriesController < ApplicationController
 
-#  skip_before_filter :require_authentification, :only => [:index, :new, :create]
+  #  skip_before_filter :require_authentification, :only => [:index, :new, :create]
 
   # GET /categories
   # GET /categories.xml
   def index
-    @categories = Category.all
+    unless current_user.has_perm?("categories_category_can_list")
+      permission_deny
+    else
+      @categories = Category.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @categories }
+      respond_to do |format|
+        format.html # index.html.erb
+        format.xml  { render :xml => @categories }
+      end
     end
   end
 
   # GET /categories/1
   # GET /categories/1.xml
   def show
-    @category = Category.find(params[:id])
+    unless current_user.has_perm?("categories_category_can_show")
+      permission_deny
+    else
+      @category = Category.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @category }
+      respond_to do |format|
+        format.html # show.html.erb
+        format.xml  { render :xml => @category }
+      end
     end
   end
 
   # GET /categories/new
   # GET /categories/new.xml
   def new
-    @category = Category.new
+    unless current_user.has_perm?("categories_category_can_create")
+      permission_deny
+    else
+      @category = Category.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @category }
+      respond_to do |format|
+        format.html # new.html.erb
+        format.xml  { render :xml => @category }
+      end
     end
   end
 
   # GET /categories/1/edit
   def edit
-    @category = Category.find(params[:id])
+    unless current_user.has_perm?("categories_category_can_update")
+      permission_deny
+    else
+      @category = Category.find(params[:id])
+    end
   end
 
   # POST /categories
   # POST /categories.xml
   def create
-    @category = Category.new(params[:category])
+    unless current_user.has_perm?("categories_category_can_create")
+      permission_deny
+    else
+      @category = Category.new(params[:category])
 
-    respond_to do |format|
-      if @category.save
-        flash[:notice] = 'Category was successfully created.'
-        format.html { redirect_to([:admin, @category]) }
-        format.xml  { render :xml => @category, :status => :created, :location => @category }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @category.errors, :status => :unprocessable_entity }
+      respond_to do |format|
+        if @category.save
+          flash[:notice] = 'Category was successfully created.'
+          format.html { redirect_to([:admin, @category]) }
+          format.xml  { render :xml => @category, :status => :created, :location => @category }
+        else
+          format.html { render :action => "new" }
+          format.xml  { render :xml => @category.errors, :status => :unprocessable_entity }
+        end
       end
     end
   end
@@ -60,16 +80,20 @@ class Admin::CategoriesController < ApplicationController
   # PUT /categories/1
   # PUT /categories/1.xml
   def update
-    @category = Category.find(params[:id])
+    unless current_user.has_perm?("categories_category_can_update")
+      permission_deny
+    else
+      @category = Category.find(params[:id])
 
-    respond_to do |format|
-      if @category.update_attributes(params[:category])
-        flash[:notice] = 'Category was successfully updated.'
-        format.html { redirect_to([:admin, @category]) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @category.errors, :status => :unprocessable_entity }
+      respond_to do |format|
+        if @category.update_attributes(params[:category])
+          flash[:notice] = 'Category was successfully updated.'
+          format.html { redirect_to([:admin, @category]) }
+          format.xml  { head :ok }
+        else
+          format.html { render :action => "edit" }
+          format.xml  { render :xml => @category.errors, :status => :unprocessable_entity }
+        end
       end
     end
   end
@@ -77,12 +101,16 @@ class Admin::CategoriesController < ApplicationController
   # DELETE /categories/1
   # DELETE /categories/1.xml
   def destroy
-    @category = Category.find(params[:id])
-    @category.destroy
+    unless current_user.has_perm?("categories_category_can_destroy")
+      permission_deny
+    else
+      @category = Category.find(params[:id])
+      @category.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(admin_categories_url) }
-      format.xml  { head :ok }
+      respond_to do |format|
+        format.html { redirect_to(admin_categories_url) }
+        format.xml  { head :ok }
+      end
     end
   end
 end
